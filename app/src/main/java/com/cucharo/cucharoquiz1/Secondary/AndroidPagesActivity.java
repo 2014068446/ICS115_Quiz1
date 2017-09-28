efc7c4edaf954aa2c492d28402cab221eeed0533
2014068446 <2014068446@ust-ics.mygbiz.com>
2017-09-28 17:02:00 +0800

    pahabol

--- app/src/main/java/com/cucharo/cucharoquiz1/Secondary/AndroidPagesActivity.java
+++ app/src/main/java/com/cucharo/cucharoquiz1/Secondary/AndroidPagesActivity.java
@@ -2,12 +2,21 @@ package com.cucharo.cucharoquiz1.Secondary;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.cucharo.cucharoquiz1.R;
 
 public class AndroidPagesActivity extends AppCompatActivity {
-
+    TextView codename,version,api,date;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pages);
+        codename = (TextView) findViewById(R.id.tvCodename);
+        version = (TextView) findViewById(R.id.tvVersion);
+        api = (TextView) findViewById(R.id.tvAPI);
+        date = (TextView) findViewById(R.id.tvDate);
+
 
     }
 }
